15772fccc5339e29eacba0d64589f190fc817ab3
David Tong <ytong82@towing.top>
2022-09-24 21:08:07 -0500

    Lintcode 2503

---
+++ 2503-implement a thread-safe counter/src/main/ThreadSafeCounter.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+
+public class ThreadSafeCounter {
+    private int i;
+    // write your code
+    
+    public ThreadSafeCounter() {
+        this.i = 0;
+        // write your code
+        
+    }
+    
+    public void incr() {
+        // write your code
+        
+    }
+    
+    public void decr() {
+        // write your code
+        
+    }
+    
+    public int getCount() {
+        return i;
+    }
+}
